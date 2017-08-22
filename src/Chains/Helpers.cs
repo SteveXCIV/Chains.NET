02b608144e7dc28deeb4aeef688abef5c623f44a
Steve Troetti <SteveXCIV@gmail.com>
2017-08-21 23:52:25 -0400

    Add basic helper methods

---
+++ src/Chains/Helpers.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Chains
+{
+    public static class Helpers
+    {
+        private static readonly string ArgumentTooSmallMessage = "Given argument was less than the specified minimum of {0}.";
+
+        private static readonly string ArgumentTooLargeMessage = "Given argument was greater than the specified maximum of {0}.";
+
+        public static TSource EnsureNotNull<TSource>(this TSource source, string name) where TSource: class
+        {
+            if (source == null)
+                throw new ArgumentNullException(name);
+            return source;
+        }
+
+        public static TSource EnsureBetweenInclusive<TSource, TCompare>(this TSource source, string name, TCompare min, TCompare max)
+            where TSource : IComparable<TCompare>
+        {
+            if (source.CompareTo(min) < 0)
+                throw new ArgumentOutOfRangeException(name, source, string.Format(ArgumentTooSmallMessage, min));
+            else if (source.CompareTo(max) > 0)
+                throw new ArgumentOutOfRangeException(name, source, string.Format(ArgumentTooLargeMessage, max));
+            return source;
+        }
+    }
+}
