ce5113c1a2b3bad98d2a98cceddd1fbe3ff10ef8
Steve Troetti <SteveXCIV@gmail.com>
2017-08-24 02:32:29 -0400

    Add CompareWith overload stub and unit tests

--- src/Chains/CompareWith.cs
+++ src/Chains/CompareWith.cs
@@ -53,5 +53,45 @@ namespace Chains
                 if (comparison != 0) return comparison;
             }
         }
+
+        /// <summary>
+        /// Compares the values in two <see cref="IEnumerable{TSource}"/>
+        /// instances, element-by-element, using a key-selection function.
+        /// </summary>
+        /// <typeparam name="TSource">
+        /// The type of the items in the source enumerables.
+        /// </typeparam>
+        /// <typeparam name="TKey">
+        /// The type of the key.
+        /// This type must implement <see cref="IComparable{T}" />.
+        /// </typeparam>
+        /// <param name="source">The source enumerable to operate on.</param>
+        /// <param name="other">The enumerable to compare to.</param>
+        /// <param name="selector">The key-selection function.</param>
+        /// <exception cref="ArgumentNullException">
+        /// The source is null.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// The other is null.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// The key-selection function is null.
+        /// </exception>
+        /// <returns>
+        /// The first non-zero value of element-by-element comparison after
+        /// applying the key-selection function, or the value -1 if the first
+        /// sequence is shorter, the value +1 if the second sequence is shorter,
+        /// or finally the value 0.
+        /// </returns>
+        public static int CompareWith<TSource, TKey>(
+            this IEnumerable<TSource> source,
+            IEnumerable<TSource> other,
+            Func<TSource, TKey> selector)
+            where TKey : IComparable<TKey>
+        {
+            selector.EnsureNotNull(nameof(selector));
+
+            throw new NotImplementedException();
+        }
     }
 }
--- test/Chains.Test/CompareWithTests.cs
+++ test/Chains.Test/CompareWithTests.cs
@@ -77,5 +77,81 @@ namespace Chains.Test
 
             Assert.IsTrue(first.CompareWith(second) == 0, ExpectedZero);
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CompareWithFunc_Should_ThrowIfSourceIsNull()
+        {
+            IEnumerable<int> first = null;
+            IEnumerable<int> second = Enumerable.Range(0, 3);
+
+            var result = first.CompareWith(second, x => (int)x);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CompareWithFunc_Should_ThrowIfOtherIsNull()
+        {
+            IEnumerable<int> first = Enumerable.Range(0, 3);
+            IEnumerable<int> second = null;
+
+            var result = first.CompareWith(second, x => (int)x);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CompareWithFunc_Should_ThrowIfSelectorIsNull()
+        {
+            IEnumerable<int> first = Enumerable.Range(0, 3);
+            IEnumerable<int> second = Enumerable.Range(0, 3);
+            Func<int, int> selector = null;
+
+            var result = first.CompareWith(second, selector);
+        }
+
+        [TestMethod]
+        public void CompareWithFunc_Should_ReturnNegativeIfFirstSequenceIsLesser()
+        {
+            var first = new List<string> { "1", "2", "3", "0" };
+            var second = new List<string> { "1", "2", "3", "4" };
+
+            Assert.IsTrue(first.CompareWith(second, int.Parse) < 0, ExpectedNegative);
+        }
+
+        [TestMethod]
+        public void CompareWithFunc_Should_ReturnNegativeIfFirstSequenceIsShorter()
+        {
+            var first = new List<string> { "1", "2", "3" };
+            var second = new List<string> { "1", "2", "3", "4" };
+
+            Assert.IsTrue(first.CompareWith(second, int.Parse) < 0, ExpectedNegative);
+        }
+
+        [TestMethod]
+        public void CompareWithFunc_Should_ReturnPositiveIfSecondSequenceIsLesser()
+        {
+            var first = new List<string> { "1", "2", "3", "4" };
+            var second = new List<string> { "1", "2", "3", "0" };
+
+            Assert.IsTrue(first.CompareWith(second, int.Parse) > 0, ExpectedPositive);
+        }
+
+        [TestMethod]
+        public void CompareWithFunc_Should_ReturnPositiveIfSecondSequenceIsShorter()
+        {
+            var first = new List<string> { "1", "2", "3", "4" };
+            var second = new List<string> { "1", "2", "3" };
+
+            Assert.IsTrue(first.CompareWith(second, int.Parse) > 0, ExpectedPositive);
+        }
+
+        [TestMethod]
+        public void CompareWithFunc_Should_ReturnZeroIfSequencesAreEqual()
+        {
+            var first = new List<string> { "1", "2", "3", "4" };
+            var second = new List<string> { "1", "2", "3", "4" };
+
+            Assert.IsTrue(first.CompareWith(second, int.Parse) == 0, ExpectedZero);
+        }
     }
 }
